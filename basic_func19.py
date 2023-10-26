4a192a0d92edda4deed90552eb21b8365f394ab9
Dalixan <samijonovdalixan@gmail.com>
2023-10-26 21:48:31 +0500

    19

--- basic_func19.py
+++ basic_func19.py
@@ -1,11 +1,8 @@
 def main(a, b):
-    '''find the absolute value of the difference between a and b. Return it.
-    
-    Args:
-        a (int): a number
-        b (int): a number
-        
-    Returns:
-        int: the result.
-    '''
-    return
+
+      return int (x)
+
+a = 4
+b = 11
+x = a - b
+print (abs(4-11))
